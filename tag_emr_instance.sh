0bcbe24175c897fa17739ffd88130595cf78dd6a
Jesse Escobedo <jesse@moda.bz>
2013-09-04 13:31:29 -0700

    updating script to read tags from a text file in S3

--- tag_emr_instance.sh
+++ tag_emr_instance.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 #
-# tag_emr_instance.sh
-#
 # Simple script to tag EMR instances from the instance itself.
 # Can (and should) be used as a bootstrap action
-#
-# Usage:
-#
-#   tag_emr_instance.sh TAG_NAME[=TAG_VALUE] TAG...
 #
 # Copyright (C) 2013 Tiago Alves Macambira <macambira (@) chaordicsystems.com>
 #
@@ -21,48 +15,56 @@
 #
 # The above copyright notice and this permission notice shall be included
 # in all copies or substantial portions of the Software.
+HADOOP_CONF='/home/hadoop/conf/core-site.xml'
+TMP_FILE='/tmp/tags.txt'
 
+function usage {
+cat << EOF
 
-set -e  # abort execution on errors
-set -x  # print what is happening
-set -o nounset  # using unset variabled will case script to fail and abort
+usage:$0 options
 
+A bash script that deploys war files to tomcat instances.
+
+OPTIONS:
+       -h      Show this messages
+       -c      The path to the tags config file in S3
+
+EOF
+}
+
+# get config
+while getopts "c:" OPTION; do
+  case $OPTION in
+    h)
+      usage
+      exit 1
+      ;;
+    c)
+      CONFIG="$OPTARG"
+      ;;
+    ?)
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+if [[ -z $CONFIG ]]; then
+  echo "Config file is not specified"
+  usage
+  exit 1
+fi
 
 # Create a separate dir to hold our junk
 INSTALL_DIR=tag_emr_instance
 mkdir --parent ${INSTALL_DIR}
 cd ${INSTALL_DIR}
 
-# Command line parsing
-
-# Grab existing credentials from core-site.xml
-HADOOP_CONF='/home/hadoop/conf/core-site.xml'
-
+# Grab existing credentials from core-site.xml and set them
 export AWS_ACCESS_KEY=`cat ${HADOOP_CONF} | grep fs.s3.awsAccessKeyId | egrep -o "<value>\w.+</value>" | sed -e 's,.*<value>\([^<]*\)</value>.*,\1,g'`
 export AWS_SECRET_KEY=`cat ${HADOOP_CONF} | grep  fs.s3.awsSecretAccessKey | egrep -o "<value>\w.+</value>" | sed -e 's,.*<value>\([^<]*\)</value>.*,\1,g'`
 
-# Aditional arguments will be used as tags to ec2-create-tags.
-TAG_ARGS=""
-for tag in "$@" ; do
-    case $tag in
-        *)
-            TAG_ARGS="${TAG_ARGS} --tag ${tag}"
-            ;;
-    esac
-done
-
-set +o nounset  # disable unset errors
-if [ -z "${AWS_ACCESS_KEY}" -o -z "${AWS_SECRET_KEY}" ]; then
-    echo "AWS access credentials not found. Aborting." > /dev/stderr
-    exit 1;
-fi
-if [ -z "${TAG_ARGS}" ]; then
-    echo "No tag provided. Aborting." > /dev/stderr
-    exit 1;
-fi
-set -o nounset  # using unset variabled will case script to fail and abort
-
-# Download and install Amazon CLI tools
+echo "Download and install Amazon CLI tools"
 wget 'http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip'
 unzip ec2-api-tools.zip
 export EC2_HOME=$(find $(pwd) -type d -name 'ec2-api-tools-*' | tail -1)
@@ -77,6 +79,16 @@ INSTANCE_ID=$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)
 # to figure out the region of this instance.
 REGION=$(hostname | cut -d. -f2)
 
-# Finally, call ec2-create-tags with the argument list constructed during
-# command line parsing.
-$EC2_CREATE_TAGS --region $REGION $INSTANCE_ID $TAG_ARGS
+if [[ $REGION == 'ec2' ]]; then
+  REGION='us-east-1'
+fi
+
+# We need to pull down the config from S3 before reading
+echo "pulling tags file from S3 for reading"
+hadoop fs -copyToLocal $CONFIG $TMP_FILE
+
+# Finally, call ec2-create-tags with the argument list form tags.txt
+echo "Reading in tags from config, and setting them via the CLI"
+while read line; do
+  $EC2_CREATE_TAGS $INSTANCE_ID --tag "$line"
+done < $TMP_FILE
